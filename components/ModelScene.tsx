9b7cf6652fb016a29902c83253c9547056da66df
haaalieee <morenoanalie14@gmail.com>
2023-05-19 00:56:52 +0800

    Fix model async and add default

--- components/ModelScene.tsx
+++ components/ModelScene.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import urlBuilder from "@/lib/urlBuilder";
-import { OrbitControls, Stage } from "@react-three/drei";
-import { Canvas, useLoader } from "@react-three/fiber";
-import { Suspense } from "react";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Loader, OrbitControls, Stage } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useEffect, useState } from "react";
+import { GLTF, GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 interface GLTFProps {
   url: string;
@@ -12,21 +12,56 @@ interface GLTFProps {
 }
 
 export default function ModelScene({ url, isFeatured }: GLTFProps) {
-  const gltf = useLoader(GLTFLoader, urlBuilder(url));
+  const [gltf, setGltf] = useState<GLTF | null>(null);
+  const defaultUrl = "./assets/keyboardver9.glb"; // Default GLTF path
+
+  useEffect(() => {
+    let currentUrl = urlBuilder(url); // Fetched or provided GLTF URL
+
+    // If the fetched URL is unavailable, use the default URL as fallback
+    if (!currentUrl) {
+      currentUrl = defaultUrl;
+    }
+
+    // Load the GLTF asynchronously
+    const loader = new GLTFLoader();
+
+    loader.load(currentUrl, setGltf, undefined, (error) => {
+      console.error("Error loading GLTF:", error);
+      setGltf(null); // Set gltf to null in case of error
+    });
+  }, [url]);
 
   return gltf ? (
-    <Canvas shadows camera={{ position: [4, 3, -10], fov: 35 }} id="canvas-body">
-      <color attach="background" args={['#292929']} />
-      <Suspense fallback={null}>
-        <Stage
-          intensity={0.2}
-          shadows={{ type: "contact", opacity: 0.8}}
-          adjustCamera={isFeatured ? 0.6 : 0.9}
-        >
-          <primitive object={gltf.scene} rotation={[0,2.75,0]} castShadow receiveShadow/>
-        </Stage>
-      </Suspense>
-      <OrbitControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI} autoRotate/>
-    </Canvas>
+    <>
+      <Canvas
+        shadows
+        camera={{ position: [4, 3, -10], fov: 35 }}
+        id="canvas-body"
+      >
+        <color attach="background" args={["#292929"]} />
+        <Suspense fallback={null}>
+          <Stage
+            intensity={0.2}
+            shadows={{ type: "contact", opacity: 0.8 }}
+            adjustCamera={isFeatured ? 0.6 : 0.9}
+          >
+            <primitive
+              object={gltf.scene}
+              rotation={[0, 2.75, 0]}
+              castShadow
+              receiveShadow
+            />
+          </Stage>
+        </Suspense>
+        <OrbitControls
+          makeDefault
+          minPolarAngle={0}
+          maxPolarAngle={Math.PI}
+          autoRotate
+        />
+      </Canvas>
+      <Loader />
+    </>
   ) : null;
 }
